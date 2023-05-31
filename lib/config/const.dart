e5ca7de44a573f24903f38e4e4757aaca0b1bed2
Dev Vanilla <mgvanillapay@gmail.com>
2023-05-31 16:29:16 +0300

    fix: Résolution bug login, num +261 refusé avant

--- lib/config/const.dart
+++ lib/config/const.dart
@@ -1,3 +1,6 @@
 class Const {
   static final String host ="https://madagroupage.com";
 }
+// class Const {
+//   static final String host ="http://192.168.88.30:3001";
+// }
--- lib/views/loginScreen.dart
+++ lib/views/loginScreen.dart
@@ -63,10 +63,14 @@ class _LoginScreenState extends State<LoginScreen> {
     return RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value);
   }
 
-  bool isPhoneNumber(String value) {
-    // Use a regular expression to check if value is a 10-digit phone number
-    return RegExp(r'^\d{10}$').hasMatch(value);
-  }
+  // bool isPhoneNumber(String value) {
+  //   // Use a regular expression to check if value is a 10-digit phone number
+  //   return RegExp(r'^\d{10}$').hasMatch(value);
+  // }
+  static bool isPhoneNumber(String value) {
+  return RegExp(r'^\+?\d{10,15}$').hasMatch(value);
+}
+
 
   void redirectionToListScreen() {
     Navigator.push(

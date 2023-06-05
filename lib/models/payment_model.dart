9388610071c43e456450ad905471e89bbe6d3c26
Dev Vanilla <mgvanillapay@gmail.com>
2023-06-05 12:18:36 +0300

    feat: ajout model lsite paiement

---
+++ lib/models/payment_model.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+
+class PaymentModel{
+
+  String reference;
+
+  double? montantEUR;
+  double? montantMGA;
+
+  DateTime? datePaiement;
+  String? modePaiement;
+
+  PaymentModel({
+    required this.reference,
+    this.datePaiement,
+
+    this.montantEUR,
+    this.montantMGA,
+
+    this.modePaiement,
+
+  });
+
+  factory PaymentModel.fromJson(Map<String, dynamic> json) {
+    
+    print("json");
+    print(json);
+
+    return PaymentModel(
+      reference: json['reference'],
+      datePaiement: DateTime.parse(json['datePaiement']),
+
+      montantEUR: json['montantEUR'] ?? 0,
+      montantMGA: json['montantMGA'] ?? 0,
+    
+      modePaiement: json['modePaiement'],
+      
+    );
+  }
+} 
